cbe87ec58421bbac1e11e25b5930d60befc57d55
Mohamed Ibrahim <mo7mad36@gmail.com>
2024-02-13 17:05:03 +0200

    added sms code

--- server/middlewares/importat_urls.js
+++ server/middlewares/importat_urls.js
@@ -1,15 +1,34 @@
+const twilio = require("twilio");
+
 module.exports = (app) => {
   app.use((req, res, next) => {
     switch (req.url) {
       case "/download/Mohamed-Ibrahim.pdf":
         // send downloaded your cv
+        send_SMS("حد نزل الملف")
+          .then(() => next())
+          .catch((_) => next());
         break;
       case "/email.png":
         // show your email
+        send_SMS("حد شاف الايميل")
+          .then(() => next())
+          .catch((_) => next());
         break;
       default:
-        return next();
+        next();
     }
     next();
   });
 };
+
+function send_SMS(msg) {
+  const accountSid = process.env.TWILIO_SID;
+  const authToken = process.env.TWILIO_TOKEN;
+
+  const client = twilio(accountSid, authToken);
+  return client.messages.create({
+    body: msg,
+    to: "+201063525389",
+  });
+}
