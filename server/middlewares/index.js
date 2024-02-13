3faecce81bfb753fa71a00da82ea8776204937d9
Mohamed Ibrahim <mo7mad36@gmail.com>
2024-02-13 16:01:43 +0200

    added cors settings

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,4 +1,4 @@
-const packages = ["bodyParser", "importat_urls", "dotenv"];
+const packages = ["cors", "bodyParser", "importat_urls", "dotenv"];
 
 module.exports = (app) =>
   packages.forEach((package) => require("./" + package)(app));
