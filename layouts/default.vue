d82888f13d9efa9c84d99d49ddf8c16f6f6d5457
Mohamed Ibrahim <mo7mad369@gmail.com>
2024-06-21 20:22:54 +0300

    edit the cv

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,22 +9,13 @@
 
 <script>
 // components
-import HeaderComponent from '@/components/layout/header/index'
-import Pointer from '@/components/layout/Pointer'
+import HeaderComponent from "@/components/layout/header/index";
+import Pointer from "@/components/layout/Pointer";
 
 export default {
-  created() {
-    console.log(
-      '%c This site UI form  %c  https://kuon.space %c Created By Mohammed Ibrahim ♥ %c',
-      'background-image: linear-gradient(90deg, #35495e, #c4a5da ) ;font-size: 2em; color: #dc080d; border-radius: 15px 0 0 15px;',
-      'background: #c4a5da;font-size: 2em; color: #333; border-radius: 0 0 0 0;',
-      'background-image: linear-gradient(90deg, #c4a5da, #4e74fd ) ;font-size: 2em; color: #f00; border-radius: 0 15px 15px 0;',
-      'background:transparent'
-    )
-  },
   components: {
     HeaderComponent,
     Pointer,
   },
-}
+};
 </script>
